8980e264bc0fc17647bb1be9c802f564edf25f18
ayamyanko <onakaayami@onakaayami-no-MacBook-Air.local>
2015-04-21 15:53:08 +0900

    a

--- mySketch/myFirstProject/src/ofApp.h
+++ mySketch/myFirstProject/src/ofApp.h
@@ -18,5 +18,10 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+    
+    static const int num = 1000;
+    ofVec3f position[num];
+    ofVec3f velocity[num];
+    ofColor color[num];
+    
 };
