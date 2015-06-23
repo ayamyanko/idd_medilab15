26254f490cc41fa2a9dc4e7fed48a8f7d0863821
ayamyanko <onakaayami@onakaayami-no-MacBook-Air.local>
2015-06-23 13:19:18 +0900

    なぜこうなった

--- example/63_TriangleMesh/src/ofApp.cpp
+++ example/63_TriangleMesh/src/ofApp.cpp
@@ -12,7 +12,9 @@ void ofApp::setup(){
     w = 200;
     h = 200;
     
-    createMesh();
+    plane=ofPlanePrimitive(400,400,20,20);
+    
+    //createMesh();
 }
 
 //--------------------------------------------------------------
@@ -42,7 +44,7 @@ void ofApp::draw(){
     
     // 頂点の位置をドットで表示
     ofTranslate(-w / 2, -h / 2);
-    mesh.drawWireframe();
+    plane.drawWireframe();
     
     cam.end(); // カメラ終了
     
